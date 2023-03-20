4a186ea260e9f62248cc9f2b5103af2b12158183
Toyaki07 <108360152+Toyaki07@users.noreply.github.com>
2023-03-20 14:02:44 +0300

    Add files via upload

---
+++ js/preloader.js
@@ -0,0 +1,34 @@
+// <-- preloader --> //
+
+window.onload = function () {
+    document.body.classList.add('loaded_hiding');
+    window.setTimeout(function () {
+      document.body.classList.add('loaded');
+      document.body.classList.remove('loaded_hiding');
+    }, 500);
+};
+
+// <-- /preloader --> //
+
+// <-- loader --> //
+function reveal() {
+    const reveals = document.querySelectorAll(".reveal");
+
+    reveals.forEach((reveal) => {
+        const windowHeight = window.innerHeight;
+        const elementTop = reveal.getBoundingClientRect().top;
+        const elementVisible = 100;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveal.classList.add("active");
+        } else {
+            reveal.classList.remove("active");
+        }
+    });
+}
+window.addEventListener("scroll", reveal);
+
+// <-- /loader --> //
+
+
+
